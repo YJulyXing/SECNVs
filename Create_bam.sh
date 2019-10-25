68443934d5cbd19276188973546ceb22430f2e25
YJulyXing <41949986+YJulyXing@users.noreply.github.com>
2019-10-25 12:20:39 -0500

    Add files via upload

--- Create_bam.sh
+++ Create_bam.sh
@@ -12,28 +12,22 @@ cd $TMP_DIR
 
 if [ $pp == "p" ]
 then
-	bwa mem -t 10 ${OUT_DIR}/${REF}.fa ${OUT_DIR}/${FILE}_1.fastq ${OUT_DIR}/${FILE}_2.fastq > ${FILE}.sam
-    samtools view -Su ${FILE}.sam | samtools sort - | samtools rmdup /dev/stdin ${FILE}.sr.rm.bam
+	bwa mem -t 10 ${OUT_DIR}/${REF}.fa ${OUT_DIR}/${FILE}_1.fastq ${OUT_DIR}/${FILE}_2.fastq \
+    -R '@RG\tID:foo\tLB:bar\tPL:illumina\tSM:Sample1\tPU:L001' | \
+    samtools view -Su | samtools sort | samtools rmdup - ${FILE}.sr.rm.bam
 else
-	bwa mem -t 10 ${OUT_DIR}/${REF}.fa ${OUT_DIR}/${FILE}.fastq > ${FILE}.sam
-    samtools view -Su ${FILE}.sam | samtools sort - | samtools rmdup -s /dev/stdin ${FILE}.sr.rm.bam
+	bwa mem -t 10 ${OUT_DIR}/${REF}.fa ${OUT_DIR}/${FILE}.fastq \
+    -R '@RG\tID:foo\tLB:bar\tPL:illumina\tSM:Sample1\tPU:L001' | \
+    samtools view -Su | samtools sort | samtools rmdup -s - ${FILE}.sr.rm.bam
 fi
 
-java -jar ${PATH_TO_PICARD}/picard.jar AddOrReplaceReadGroups \
-    I= ${FILE}.sr.rm.bam \
-    O= ${FILE}.sr.rm.hd.bam \
-    SORT_ORDER=coordinate \
-    RGID=foo \
-    RGLB=bar \
-    RGPL=illumina \
-    RGSM=Sample1 \
-    RGPU=L001 \
-    VALIDATION_STRINGENCY=LENIENT \
-    CREATE_INDEX=True
+samtools index ${FILE}.sr.rm.bam
 
 java -jar ${PATH_TO_GATK}/GenomeAnalysisTK.jar -T RealignerTargetCreator \
--R ${OUT_DIR}/${REF}.fa -I ${FILE}.sr.rm.hd.bam -o ${FILE}_IndelRealigner.intervals
+-R ${OUT_DIR}/${REF}.fa -I ${FILE}.sr.rm.bam -o ${FILE}_IndelRealigner.intervals
 
 java -jar ${PATH_TO_GATK}/GenomeAnalysisTK.jar --filter_bases_not_stored -T IndelRealigner \
--R ${OUT_DIR}/${REF}.fa -I ${FILE}.sr.rm.hd.bam -targetIntervals ${FILE}_IndelRealigner.intervals \
+-R ${OUT_DIR}/${REF}.fa -I ${FILE}.sr.rm.bam -targetIntervals ${FILE}_IndelRealigner.intervals \
 -o ${OUT_DIR}/${FILE}.final.bam
+
+samtools flagstat ${OUT_DIR}/${FILE}.final.bam
